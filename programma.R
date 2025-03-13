6ff759bf3f47eeb745ab622a8512a82b15deb26a
Eriks_Baumanis_231RDC032 <144455774+Erik870@users.noreply.github.com>
2025-03-13 11:21:20 +0000

    2

--- programma.R
+++ programma.R
@@ -11,3 +11,14 @@ kordat <- read.table("variants12.txt", header = TRUE, sep = "\t", stringsAsFacto
 
 # 3. Pārveido kolonnas no 9. un uz augšu par faktoriem
 kordat[, 9:ncol(kordat)] <- lapply(kordat[, 9:ncol(kordat)], factor)
+
+# 4. Izvada faktoru līmeņu kopsavilkumu uz failu results.txt
+summary_table <- summary(kordat[, 9:ncol(kordat)])
+write.table(summary_table, file = "results.txt", append = FALSE, sep = "\t", col.names = NA)
+
+# 5. Saskalda Slope kolonnu sarakstā pēc b faktora vērtībām un saglabā to sl.by.b
+sl.by.b <- split(kordat$Slope, kordat$b)
+write.table(sl.by.b, file = "results.txt", append = TRUE, sep = "\t", col.names = NA)
+
+# 6. Izveido jaunu kolonnu "Average", kas satur vidējo no "Slope", "Intercept" un "adj.r.squared"
+kordat$Average <- rowMeans(kordat[, c("Slope", "Intercept", "adj.r.squared")])
